21fae74ae8f9965b0aa57ce63f5fab8a8402db1f
Paula <paula.suarez@sundevs.com>
2023-03-19 01:00:06 -0400

    First commit

---
+++ course1restAPI/src/addTodo.js
@@ -0,0 +1,37 @@
+const { v4 } = require("uuid");
+const AWS = require("aws-sdk");
+const middy = require("@middy/core");
+const httpJsonBodyParser = require("@middy/http-json-body-parser");
+
+const addTodo = async (event) => {
+  const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+  const { todo } = event.body;
+  const createAt = new Date().toISOString();
+  const id = v4();
+
+  console.log("This is an id", id);
+
+  const newTodo = {
+    id,
+    todo,
+    createAt,
+    completed: false,
+  };
+
+  await dynamoDB
+    .put({
+      TableName: "TodoTable",
+      Item: newTodo,
+    })
+    .promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(newTodo),
+  };
+};
+
+module.exports = {
+  handler: middy(addTodo).use(httpJsonBodyParser()),
+};
---
+++ course1restAPI/src/fetchTodos.js
@@ -0,0 +1,24 @@
+const { v4 } = require("uuid");
+const AWS = require("aws-sdk");
+
+const fetchTodos = async (event) => {
+  const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+  let todos;
+
+  try {
+    const results = await dynamoDB.scan({ TableName: "TodoTable" }).promise();
+    todos = results.Items;
+  } catch (error) {
+    console.log(error);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(todos),
+  };
+};
+
+module.exports = {
+  handler: fetchTodos,
+};
